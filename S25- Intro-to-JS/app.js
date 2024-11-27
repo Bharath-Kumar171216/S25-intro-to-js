fe3fce58e02fef761f4a7be0223dfd2de8d635f5
Bharath Kumar <bharathveeravalli786@gmail.com>
2024-11-27 23:21:25 +0530

    Add files via upload

---
+++ S25- Intro-to-JS/app.js
@@ -0,0 +1,4 @@
+let myHeading = document.createElement("h1");
+myHeading.innerHTML = "This heading is created and beautified using java script"
+myHeading.style.color = "red";
+document.body.append(myHeading);
